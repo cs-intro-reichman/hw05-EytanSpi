1ca5c5a4af2cc13817ee822aaadb440e625659ff
Eytan Spialter <eytan.spialter@runi.ac.il>
2024-01-17 18:40:34 +0200

    aesthetic changes

--- GameOfLife.java
+++ GameOfLife.java
@@ -6,8 +6,6 @@ public class GameOfLife {
 
 	public static void main(String[] args) {
 		String fileName = args[0];
-		//// Uncomment the test that you want to execute, and re-compile.
-		//// (Run one test at a time).
 		//// test1(fileName);
 		//// test2(fileName);
 		//// test3(fileName, 3);
